009e572f4a36401919b130c3e5a196e1db1de343
Luiz Mello - Turma 06 <luizmacmillan@gmail.com>
2020-07-13 19:07:17 -0300

    Adição do arquivo exercise4.js que retorna se é positive, negative ou zero.

---
+++ exercises/4_1/exercise4.js
@@ -0,0 +1,14 @@
+let a = -10;
+let resultado = null;
+
+if (a < 0) {
+  resultado = "negative";
+}
+else if (a > 0) {
+  resultado = "positive";
+}
+else {
+  resultado = "zero"
+}
+
+console.log(resultado)
