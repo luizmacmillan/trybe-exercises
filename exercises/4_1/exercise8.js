480622adb4ad364cc1bea07a29b2dc601a1ca507
Luiz Mello - Turma 06 <luizmacmillan@gmail.com>
2020-07-13 22:10:49 -0300

    Adição do arquivo exercise8.js que retorna true caso um dos 3 números é par.

---
+++ exercises/4_1/exercise8.js
@@ -0,0 +1,13 @@
+const num1 = 11;
+const num2 = 10;
+const num3 = 6;
+let temPar = false;
+
+if ((num1 % 2 == 0) || (num1 % 2 == 0) || (num1 % 2 == 0)) {
+  temPar = true;
+} 
+else {
+  temPar = false;
+}
+
+console.log(temPar);
