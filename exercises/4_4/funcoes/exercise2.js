82a99e85d8e8f8d9a7561c598871f539dcb70531
Luiz Mello - Turma 06 <luizmacmillan@gmail.com>
2020-07-16 19:39:03 -0300

    Incremento das restrições nos ifs dos arquivos funcoes/exercise2.js (linhas 7 e 9) e funcoes/exercise3.js (linhas 7 e 9) para evitar possíveis erros.

--- exercises/4_4/funcoes/exercise2.js
+++ exercises/4_4/funcoes/exercise2.js
@@ -1,12 +1,12 @@
 const numbers = [2, 3, 6, 7, 10, 1];
 
 function maiorNumero(param) {
-  let maior;
+  let maior = param[0];
   for (index1 = 0; index1 < param.length; index1 += 1 ) {
     for (index2 = index1 + 1; index2 < param.length; index2 += 1) {
-      if (param[index1] >= param[index2]) {
+      if (param[index1] >= param[index2] && maior <= param[index1]) {
         maior = index1;
-      } else {
+      } else if (param[index1] <= param[index2] && maior <= param[index2]) {
         maior = index2;
       }
     }
