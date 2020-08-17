18ce245703af981c63636df905e5238302864b3e
Luiz Mello - Turma 06 <luizmacmillan@gmail.com>
2020-08-17 19:38:45 -0300

    Exercício 5.

--- exercises/8_1/exercise5.js
+++ exercises/8_1/exercise5.js
@@ -67,6 +67,7 @@ const expected_result = false
 
 function everyoneWasBornOnSecXX() {
   // escreva seu código aqui
+  return books.every((book) => book.author.birthYear >= 1901 && book.author.birthYear <= 2000);
 }
 
 assert.equal(everyoneWasBornOnSecXX(), expected_result);
