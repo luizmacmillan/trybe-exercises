a58eb5c43940ce367b6689a034dca810a4dc624a
Luiz Mello - Turma 06 <luizmacmillan@gmail.com>
2020-08-20 20:10:52 -0300

    Exercício 3.

--- exercises/8_4/exercise3.js
+++ exercises/8_4/exercise3.js
@@ -1,21 +1,27 @@
 const assert = require('assert')
 
 const alex = {
-    name: "Alex",
-    age: 26,
-    likes: ["fly fishing"],
-    nationality: "Australian"
+  name: "Alex",
+  age: 26,
+  likes: ["fly fishing"],
+  nationality: "Australian"
 }
 
 const gunnar = {
-    name: "Gunnar",
-    age: 30,
-    likes: ["hiking", "scuba diving", "taking pictures"],
-    nationality: "Icelandic"
+  name: "Gunnar",
+  age: 30,
+  likes: ["hiking", "scuba diving", "taking pictures"],
+  nationality: "Icelandic"
 }
 
 // complete a assinatura da função abaixo
-const personLikes = () => `${name} is ${age} years old and likes ${likes.join(", ")}.`
+const personLikes = (person) => {
+  const { name } = person;
+  const { age } = person;
+  const { likes } = person;
+
+  return `${name} is ${age} years old and likes ${likes.join(", ")}.`
+}
 assert.equal(personLikes(alex), "Alex is 26 years old and likes fly fishing.")
 assert.equal(personLikes(gunnar), "Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.")
 
