e73f2de507697e5e3e486bba245086809240c9de
Luiz Mello - Turma 06 <luizmacmillan@gmail.com>
2020-08-20 20:34:05 -0300

    Exercício 7.

--- exercises/8_4/exercise7.js
+++ exercises/8_4/exercise7.js
@@ -19,6 +19,8 @@ const ships = [
 
 // escreva shipLength abaixo
 
+const shipLength = ({name, length, measurementUnit = 'meters'}) => `${name} is ${length} ${measurementUnit} long`;
+
 assert.equal(shipLength(ships[0]), "Titanic is 269.1 meters long")
 assert.equal(shipLength(ships[1]), "Queen Mary 2 is 1132 feet long")
 assert.equal(shipLength(ships[2]), "Yamato is 256 meters long")
