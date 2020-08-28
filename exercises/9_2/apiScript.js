aa2f37e74b9f6e2a6519b345b18b04baf33bc839
Luiz Mello - Turma 06 <luizmacmillan@gmail.com>
2020-08-27 22:16:46 -0300

    Exercício 1.

--- exercises/9_2/apiScript.js
+++ exercises/9_2/apiScript.js
@@ -1,7 +1,19 @@
 const API_URL = 'https://icanhazdadjoke.com/';
 
+const myObject = {
+  method: 'GET',
+  headers: { 'Accept': 'application/json' }
+};
+
+fetch(API_URL, myObject)
+  .then(response => response.json())
+  .then(data => {
+    const jokeContainer = document.querySelector('#jokeContainer');
+    jokeContainer.innerText = data.joke;
+  });
+
 const fetchJoke = () => {
-  // Adicionar lógica aqui!
+  
 };
 
 window.onload = () => fetchJoke();
