12e2e8394b3c5a263cd9219c8cee17330259171c
Luiz Mello - Turma 06 <luizmacmillan@gmail.com>
2020-09-02 19:03:17 -0300

    Adicionando arquivos .test para a resolução dos exercícios do dia 10.1.

---
+++ exercises/10_1/exercise5.test.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+
+const names = [
+  'Aanemarie',  'Adervandes',   'Akifusa',
+  'Abegildo',   'Adicellia',    'Aladonata',
+  'Abeladerco', 'Adieidy',  'Alarucha',
+];
+
+function containsA() {
+  // escreva seu código aqui
+  return names.reduce((aTimes, currentName) => aTimes + currentName.split('').reduce((aTimesAtCurrentName, currentNameLetter) => (currentNameLetter.toLocaleLowerCase() === 'a') ? aTimesAtCurrentName + 1 : aTimesAtCurrentName, 0), 0);
+}
+
+assert.deepEqual(containsA(), 20);
+
+// Dada o array de nomes, retorne a quantidade de vezes em que aparecem a letra a maiúscula ou minúscula.
