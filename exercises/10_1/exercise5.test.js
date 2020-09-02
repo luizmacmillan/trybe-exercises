03d7117f74f34608eeca1d4724acdf8c12889a69
Luiz Mello - Turma 06 <luizmacmillan@gmail.com>
2020-09-02 20:35:42 -0300

    Exercício 5.

--- exercises/10_1/exercise5.test.js
+++ exercises/10_1/exercise5.test.js
@@ -1,3 +1,13 @@
 const { obj1, obj2, obj3 } = require('./exercise5');
 
-// Compare dois objetos (JSON) para verificar se são idênticos ou não
+describe('Compare dois objetos (JSON) para verificar se são idênticos ou não.', () => {
+  it('obj1 e obj2.', () => {
+    expect(obj1).toEqual(obj2);
+  });
+  it('obj1 e obj3.', () => {
+    expect(obj1).not.toEqual(obj3);
+  });
+  it('obj2 e obj3.', () => {
+    expect(obj2).not.toEqual(obj3);
+  });
+})
