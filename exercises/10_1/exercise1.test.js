cfe970c3261f4595defb94d42f126e288533069f
Luiz Mello - Turma 06 <luizmacmillan@gmail.com>
2020-09-02 19:36:26 -0300

    Exercício 1.

--- exercises/10_1/exercise1.test.js
+++ exercises/10_1/exercise1.test.js
@@ -5,12 +5,16 @@ describe('A função sum(a, b) retorna a soma do parâmetro a com o b.', () => {
     expect(sum(4, 5)).toBe(9);
   });
   it('Se o retorno de sum(0, 0) é 0.', () => {
-
+    expect(sum(0, 0)).toBe(0);
   });
   it('Se a função sum lança um erro quando os parâmetros são 4 e "5"(string 5).', () => {
-
+    expect(() => {
+      sum(4, '5')
+    }).toThrow();
   });
   it('Se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5").', () => {
-
+    expect(() => {
+      sum(4, '5')
+    }).toThrow('parameters must be numbers');
   });
 })
