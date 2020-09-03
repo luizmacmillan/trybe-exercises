57238e4a3195c67375f8625c4d6837bf974a34cf
Luiz Mello - Turma 06 <luizmacmillan@gmail.com>
2020-09-02 21:23:33 -0300

    Parte 2 - Exercício 3.

--- exercises/10_1/jest-exercises/reverseint/index.js
+++ exercises/10_1/jest-exercises/reverseint/index.js
@@ -9,7 +9,21 @@
 //   reverseInt(-90) === -9
 
 function reverseInt(n) {
-
+  function invertString(str) {
+    let invertedStr = str[str.length - 1];
+    for (let i = str.length - 2; i >= 0; i -= 1) {
+      invertedStr = `${invertedStr}${str[i]}`;
+    }
+    return invertedStr;
+  }
+  if (n < 0) {
+    let stringedNumber = `${n * -1}`;
+    return parseInt(invertString(stringedNumber)) * -1;
+  } else if (n === 0) {
+    return 0;
+  } else {
+    return parseInt(invertString(`${n}`));
+  }
 }
 
 module.exports = reverseInt;
