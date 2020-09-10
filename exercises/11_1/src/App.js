92b6f3580affb70ffe1117a8521caa2b11839e19
Luiz Mello - Turma 06 <luizmacmillan@gmail.com>
2020-09-10 19:09:38 -0300

    Adicionada a função task.

--- exercises/11_1/src/App.js
+++ exercises/11_1/src/App.js
@@ -2,6 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
+
 function App() {
   return (
     <div className="App">
